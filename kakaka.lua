29957e8fa36285f41daa16403ae3bedd34a88e54
acunpro <42316587+acunpro@users.noreply.github.com>
2018-08-25 23:45:44 +0700

    Update kakaka.lua

--- kakaka.lua
+++ kakaka.lua
@@ -1,8 +1,14 @@
 gg.toast(os.date([[
-Today: %Y/%m/%d Time: %H:%M:%S
-www.youtube.com/c/ArsyahG]]))
-gg.alert("@𝐎𝐫𝐚𝐧𝐠𝐁𝐢𝐚𝐬𝟒")
+Today: %Y/%m/%d Time: %H:%M:%S]]))
+gg.alert("VVIP 0.7.1")
+gg.alert("PUBG Mobile")
+gg.toast("FUNNY MOMENT")
+
+::START::
 gg.alert("Untuk VIP Member hubungin saya Karna Saya akan Membuat Gruop vip \n dan Memindahkan coding ke server baru karna script sudah terbongkar Thanks")
 gg.alert("dear VIP Member contact me because I will make VIP Group \n and Move coding to the new server because the script has been uncovered \n Thanks")
 gg.alert("https://t.me/@OrangBias4")
-os.exit
+goto sdone
+
+::sdone::
+os.exit()
