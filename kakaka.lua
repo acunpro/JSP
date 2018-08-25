429813ac73972a3bfc6e4621359303519aaf61c8
acunpro <42316587+acunpro@users.noreply.github.com>
2018-08-25 23:41:02 +0700

    Update kakaka.lua

--- kakaka.lua
+++ kakaka.lua
@@ -5,5 +5,5 @@ www.youtube.com/c/ArsyahG]]))
 gg.alert("@𝐎𝐫𝐚𝐧𝐠𝐁𝐢𝐚𝐬𝟒")
 gg.alert("Untuk VIP Member hubungin saya Karna Saya akan Membuat Gruop vip \n dan Memindahkan coding ke server baru karna script sudah terbongkar Thanks")
 gg.alert("dear VIP Member contact me because I will make VIP Group \n and Move coding to the new server because the script has been uncovered \n Thanks")
-
+gg.alert("https://t.me/@OrangBias4")
 ::exit
