5441203fad75ee1bf5c67cf82d1568a81af9da4c
acunpro <42316587+acunpro@users.noreply.github.com>
2018-08-25 23:19:52 +0700

    Update kakaka.lua

--- kakaka.lua
+++ kakaka.lua
@@ -2,7 +2,8 @@ gg.toast(os.date([[
 Today: %Y/%m/%d Time: %H:%M:%S
 www.youtube.com/c/ArsyahG]]))
 gg.alert("@𝐎𝐫𝐚𝐧𝐠𝐁𝐢𝐚𝐬𝟒")
-gg.toast("www.youtube.com/c/ArsyahG")
+gg.toast("Untuk VIP Member hubungin saya Karna Saya akan Membuat Gruop vip \n dan Memindahkan coding ke server baru karna script sudah terbongkar Thanks")
+gg.toast("dear VIP Member contact me because I will make VIP Group \n and Move coding to the new server because the script has been uncovered \n Thanks")
 
 function BPSS()
 gg.searchNumber("1.2F;1.8F:9::1", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
